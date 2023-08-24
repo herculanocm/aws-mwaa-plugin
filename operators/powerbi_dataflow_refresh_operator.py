ae91aca97431386cef38c3014aa19287d8587da9
lucasteodorovital <lucasteodorovital@hotmail.com>
2023-08-24 17:48:38 -0300

    Ajustando operador de att dataflow PBI

--- operators/powerbi_dataflow_refresh_operator.py
+++ operators/powerbi_dataflow_refresh_operator.py
@@ -1,6 +1,7 @@
 import json
 import urllib3
 import time
+from datetime import datetime
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
@@ -16,10 +17,12 @@ class PowerBIDataflowRefreshOperator(BaseOperator):
         dataflow_id,
         workspace_id,
         refresh_endpoint='refreshes',
+        transactions_endpoint='transactions',
         api_version='v1.0',
         token=None,
-        polling_interval=30,  # Time in seconds to wait before polling for refresh status
-        max_polling_attempts=30,  # Maximum number of attempts to poll for refresh status
+        polling_interval=6,  # Time in seconds to wait before polling for refresh status
+        max_polling_attempts=50,  # Maximum number of attempts to poll for refresh status
+        format_string = '%Y-%m-%dT%H:%M:%S.%fZ',
         *args, **kwargs
     ):
         """
@@ -48,7 +51,8 @@ class PowerBIDataflowRefreshOperator(BaseOperator):
         self.token = token
         self.polling_interval = polling_interval
         self.max_polling_attempts = max_polling_attempts
-        
+        self.transactions_endpoint = transactions_endpoint
+        self.format_string = format_string
 
     def _get_refresh_status(self):
         
@@ -59,12 +63,12 @@ class PowerBIDataflowRefreshOperator(BaseOperator):
         
         # Construct the API endpoint
         base_url = f'https://api.powerbi.com/{self.api_version}/myorg/groups/{self.workspace_id}/'
-        endpoint_url = f'{base_url}dataflows/{self.dataflow_id}/{self.refresh_endpoint}'
-        print(f"Refresh dataflow url: {endpoint_url} - headers: {headers}")
+        refresh_endpoint_url = f'{base_url}dataflows/{self.dataflow_id}/{self.refresh_endpoint}'
+        print(f"Refresh dataflow url: {refresh_endpoint_url} - headers: {headers}")
 
         # Send the GET request to fetch the refresh status
         http = urllib3.PoolManager()
-        self.log.info(f"Refresh dataflow url: {endpoint_url}")
+        self.log.info(f"Refresh dataflow url: {refresh_endpoint_url}")
 
         body_data = {
             "notifyOption": "MailOnFailure"
@@ -73,12 +77,38 @@ class PowerBIDataflowRefreshOperator(BaseOperator):
         json_data = json.dumps(body_data)
         response = http.request(
             'POST', 
-            endpoint_url, 
+            refresh_endpoint_url, 
             body=json_data,
             headers=headers
             )
         self.log.info(f"status: {response.status}, data: {response.data}")
         return response.status
+
+    def _get_transaction_list(self):
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.token}'
+        }
+
+        base_url = f'https://api.powerbi.com/{self.api_version}/myorg/groups/{self.workspace_id}/'
+        transactions_endpoint_url = f'{base_url}dataflows/{self.dataflow_id}/{self.transactions_endpoint}'
+        print(f"Refresh dataflow url: {transactions_endpoint_url} - headers: {headers}")
+        
+        http = urllib3.PoolManager()
+        body_data = {
+            "notifyOption": "MailOnFailure"
+        }
+
+        json_data = json.dumps(body_data)
+        response = http.request(
+            'POST', 
+            transactions_endpoint_url, 
+            body=json_data,
+            headers=headers
+            )
+        self.log.info(f"status: {response.status}, data: {response.data}")
+        return response
+
 
     def execute(self, context):
         self.log.info(f"Calling API Dataflow ID: {self.dataflow_id}")
@@ -88,19 +118,37 @@ class PowerBIDataflowRefreshOperator(BaseOperator):
             self.log.info(f"Power BI dataflow refresh completed for Dataflow ID: {self.dataflow_id}")
         else:
             raise Exception(f"Power BI dataflow refresh failed for Dataflow ID: {self.dataflow_id}")
-        
-        # Wait for the refresh to complete
-        # attempts = 0
-        # while attempts < self.max_polling_attempts:
-        #     self.log.info(f"attempts: {attempts}")
-        #     refresh_status = self._get_refresh_status()
-        #     if refresh_status.get('status', {}).get('value') == 'Succeeded':
-        #         self.log.info(f"Power BI dataflow refresh completed for Dataflow ID: {self.dataflow_id}")
-        #         break
-        #     elif refresh_status.get('status', {}).get('value') == 'Failed':
-        #         self.log.error(f"Power BI dataflow refresh failed for Dataflow ID: {self.dataflow_id}")
-        #         raise Exception(f"Power BI dataflow refresh failed for Dataflow ID: {self.dataflow_id}")
-        #     else:
-        #         self.log.info(f"Waiting for Power BI dataflow refresh to complete. Attempt {attempts+1}/{self.max_polling_attempts}")
-        #         attempts += 1
-        #         time.sleep(self.polling_interval)
+
+        for attempt in range(1, self.max_polling_attempts + 1):
+            time.sleep(self.polling_interval)
+
+
+            try:
+                response = self._get_transaction_list()
+                if response.status < 200 or response.status > 299:
+                    self.log.warning(f"Error checking Power BI refresh status: {response.status}")
+                else:               
+                    arr = json.loads(response.data.decode("utf-8")).get("value")
+                    max_value = datetime.strptime('1999-12-01T02:54:54.86Z', self.format_string)
+                    max_index = -1
+
+                    for index, item in enumerate(arr):
+                        _date = datetime.strptime(item["startTime"], self.format_string)
+                        if _date > max_value:
+                            max_value = _date
+                            max_index = index
+
+                    ultima_data = arr[max_index]
+                    refresh_status = ultima_data.get("status")
+                    
+                    self.log.info(f"Refresh attempt {attempt}/{self.max_polling_attempts} - Status: {refresh_status}")
+                    if refresh_status == "Success":
+                        print("Power BI dataset refresh completed.")
+                        self.log.info("Power BI dataset refresh completed.")
+                        return
+
+            except Exception as e:
+                self.log.warning(f"Error checking Power BI refresh status: {e}")
+
+        self.log.warning("Power BI dataset refresh did not complete within the specified attempts.")
+    
